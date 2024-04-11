a30135962b13f477e664fd11901b2f95634aefde
rohit003kumar <151962481+rohit003kumar@users.noreply.github.com>
2024-04-11 13:00:34 +0000

    basic

---
+++ basic/basic.js
@@ -0,0 +1,10 @@
+const id = 1234;
+let userName = "rohit";
+var password = "1234";
+city = "patna";
+
+userName = "rksingh";
+password = "002";
+
+console.log(id);
+console.table([userName,password,city]);
